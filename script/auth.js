116dd44662428c7683e2f7655388b97da01231a1
TrimMuharremi <38293142+TrimMuharremi@users.noreply.github.com>
2020-01-04 01:18:51 +0100

    Gallery site

--- script/auth.js
+++ script/auth.js
@@ -1,18 +1,30 @@
+var ch1 = 0;
+var ch2 = 0;
+
 function check() {
+  ch2 = 0;
   var x = document.getElementById("fullname");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+  if (ch1 == 0) {
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    ch1 += 1;
   }
 }
 
 function check2() {
+  ch1 = 0;
+
   var x = document.getElementById("fullname");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
+  if (ch2 == 0) {
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    ch2 += 1;
   }
 }
 
--- script/booking.js
+++ script/booking.js
@@ -1,61 +1,61 @@
 var map, infoWindow;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: -34.397,
-      lng: 150.644
-    },
-    zoom: 13
-  });
-  infoWindow = new google.maps.InfoWindow();
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 13
+    });
+    infoWindow = new google.maps.InfoWindow();
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-      function(position) {
-        var pos = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude
-        };
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
 
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(pos);
-      },
-      function() {
-        handleLocationError(true, infoWindow, map.getCenter());
-      }
-    );
-  } else {
-    handleLocationError(false, infoWindow, map.getCenter());
-  }
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location found.");
+                infoWindow.open(map);
+                map.setCenter(pos);
+            },
+            function () {
+                handleLocationError(true, infoWindow, map.getCenter());
+            }
+        );
+    } else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 }
 
 if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(success, error);
+    navigator.geolocation.getCurrentPosition(success, error);
 } else {
-  alert("location not supported");
+    alert("location not supported");
 }
 
 //callbacks
 function error(msg) {
-  alert("error in geolocation");
+    alert("error in geolocation");
 }
 
 function success(position) {
-  var lats = position.coords.latitude;
-  var lngs = position.coords.longitude;
-  alert(lats);
-  alert(lngs);
+    var lats = position.coords.latitude;
+    var lngs = position.coords.longitude;
+    alert(lats);
+    alert(lngs);
 }
