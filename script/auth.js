ddbff4c9eea58d1c41b983eca46f77e5e52d8dcc
Drilon <drilonpa@gmail.com>
2020-01-04 04:05:11 +0100

    Update

--- script/auth.js
+++ script/auth.js
@@ -1,48 +1,111 @@
-var ch1 = 0;
-var ch2 = 0;
+var checked = true;
 
-function check() {
-  ch2 = 0;
-  var x = document.getElementById("fullname");
-  if (ch1 == 0) {
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-    ch1 += 1;
-  }
-}
-
-function check2() {
-  ch1 = 0;
-
-  var x = document.getElementById("fullname");
-  if (ch2 == 0) {
+function checkLogin() {
+  if (checked) {
+    var x = document.getElementById("fullname");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-    ch2 += 1;
+    checked = false;
   }
 }
 
-// function checkPassword() {
-//   var x = document.getElementById("password").nodeValue();
-//   console.log(x);
-// }
-// var x = document.getElementById("password").value;
-// console.log(x);
+function checkSignup() {
+  if (!checked) {
+    var x = document.getElementById("fullname");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+    checked = true;
+  }
+}
 
-function test() {
-  var x = document.getElementById("password").value;
+function returnDefault() {
+  document.getElementById("email").innerHTML = "";
+  document.getElementById("password").value = "";
+  document.getElementById("name").innerHTML = "";
+}
 
-  var pss = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+function checkEmail(email) {
+  let x = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < x.length; i++) {
+    if (x[i].email == email) return true;
+  }
+  return false;
+}
 
-  if (!x.match(pss)) {
-    alert(
-      "Passwordi duhet te kete : Nje shkronje te madhe , nje e vogel, nje numer ,6-20 karaktere"
-    );
+function checkInfo(email, password) {
+  let x = JSON.parse(localStorage.getItem("users"));
+  for (let i = 0; i < x.length; i++) {
+    if (x[i].email == email && x[i].password == password) return true;
+  }
+  return false;
+}
+
+let loged = false;
+
+function logIn() {
+  event.preventDefault();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  if (!loged) {
+    if (checkInfo(email, password)) {
+      loged = true;
+      window.location.href = "main.html";
+    } else if (!checkEmail(email)) {
+      document.getElementById("em1").innerHTML = "E-mail is not registred !";
+      document.getElementById("em1").style.color = "red";
+    } else {
+      document.getElementById("pas1").innerHTML = "Password is incorrect";
+      document.getElementById("pas1").style.color = "red";
+    }
+  }
+}
+
+function signUp() {
+  event.preventDefault();
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let fullName = document.getElementById("name").value;
+
+  returnDefault();
+
+  let users = {
+    fullName: fullName,
+    email: email,
+    password: password
+  };
+
+  let getUsers = JSON.parse(localStorage.getItem("users"));
+  if (!getUsers) {
+    getUsers = new Array();
+  }
+  if (checkEmail(email)) {
+    alert("This e-mail is used earlier");
+    return;
+  }
+  let p = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+  if (!password.match(p)) {
+    alert("Password should be 6-20 chars , have : A-Z , a-z , 1-9");
+    return;
+  }
+  getUsers.push(users);
+
+  localStorage.setItem("users", JSON.stringify(getUsers));
+  checkLogin();
+}
+
+function checkWhich() {
+  if (!checked) {
+    logIn();
+    checkLogin();
+  } else {
+    resetDefault();
+    signUp();
   }
 }
